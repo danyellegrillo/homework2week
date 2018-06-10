3d5e4d2e8266b9db91d41a4c39165d3244aa7cd7
DANYELLEGRILLO <dany_varre@hotmail.com>
2018-06-10 15:18:37 +0200

    starting the pathfind function, the recurtion is almost

--- src/pathFind.js
+++ src/pathFind.js
@@ -1,8 +1,17 @@
 
-function pathFind(path, object){
-
+function pathFind(path, object) {
+  if (object.hasOwnProperty(path[0])){
+     return pathFind(path.slice(1), object[path[0]])
+  }
+  else return object.path.length
 }
 
-
-
-module.exports = { pathFind } 
+module.exports = { pathFind }
+// const obj = {
+//   foo: "Hey"
+// }
+// assert.strictEqual(
+//   pathFind(["foo"], obj),
+//   "Hey",
+//   "should return the foo property"
+// //
